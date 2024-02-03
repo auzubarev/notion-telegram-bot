79989df7340c9646b2197d27406715ef583da9bd
Alex Zubarev <auzubarev@gmail.com>
2024-02-03 12:07:46 +0700

    Request to create a page in Notion

--- notion_bot.php
+++ notion_bot.php
@@ -2,19 +2,45 @@
 
 require_once(__DIR__ . '/config.php');
 
+function post($url, $params=[], $headers=[], $timeout = 5) {
+    $curl = curl_init( $url );
+    $params = json_encode($params);
+    $headers[] = 'Content-Type: application/json;charset=utf-8';
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt( $curl, CURLOPT_POSTFIELDS, $params );
+    curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt( $curl, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, true);
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 if ($_SERVER['HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN'] !== TG_SECRET || !$data['message']['text'] || !$data['message']['chat']['id'])
     exit;
 
-$text = $data['message']['text'];
-$ch = curl_init('https://api.telegram.org/bot' . TG_TOKEN . '/sendMessage');
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, [
-    'chat_id' => $data['message']['chat']['id'],
-    'text' => $text
-]);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-$res = curl_exec($ch);
-curl_close($ch);
+$page = post('https://api.notion.com/v1/pages', [
+    'parent' => [
+        "type" => "database_id",
+        "database_id" =>  NOTION_DATABASE
+    ],
+    "properties" => [
+        "Name" => [
+            "title" => [
+                [
+                    "text" => ["content" => $data['message']['text']]
+                ]
+            ]
+        ]
+    ]
+], ["Authorization: Bearer ".NOTION_TOKEN, "Notion-Version: 2022-06-28"]);
+
+post('https://api.telegram.org/bot' . TG_TOKEN . '/sendMessage',
+    [
+        'chat_id' => $data['message']['chat']['id'],
+       'text' => $page['id']?"✅ Сохранено":"❌ Ошибка"
+    ]
+);
